27fbc2f33e18899a7a7c8022dbb00d6f78e15859
Juan Ramallo <ra-juancho@hotmail.com>
2014-01-25 12:04:30 -0400

    Initial Commit

---
+++ SavingData SIMON ALLRADICE/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SavingData SIMON ALLRADICE
+//
+//  Created by Juan Manuel on 1/25/14.
+//  Copyright (c) 2014 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SavingData SIMON ALLRADICE/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SavingData SIMON ALLRADICE
+//
+//  Created by Juan Manuel on 1/25/14.
+//  Copyright (c) 2014 Home. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
